bcfd5de304d2f57a7569c2414741e27400341242
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-10-03 03:02:26 +0300

    01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = [];
+galleryItems.map(markupCreateFunction);
+function markupCreateFunction(obj) {
+  markup.push(`
+  <li class="gallery__item">
+  <a class="gallery__link" href="${obj.original}">
+  
+  <img
+  class="gallery__image"
+  src="${obj.preview}"
+  data-source="${obj.original}"
+   alt="${obj.description}" 
+   />
+  </a>
+  </li>`);
+}
+gallery.insertAdjacentHTML("afterbegin", markup.join(""));
+gallery.addEventListener("click", handlerClick);
+function handlerClick(evt) {
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+}
