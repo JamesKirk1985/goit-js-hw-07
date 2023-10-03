d8f52eb395d9e1727fe1b3bdf0ace80741928d09
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-10-03 03:32:42 +0300

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markup = [];
+galleryItems.map(markupCreateFunction);
+function markupCreateFunction(obj) {
+  markup.push(`
+  <li class="gallery__item">
+   <a class="gallery__link" href="${obj.original}">
+      <img class="gallery__image" src="${obj.preview}" alt="${obj.description}" />
+   </a>
+</li>
+  `);
+}
+gallery.insertAdjacentHTML("afterbegin", markup.join(""));
+gallery.addEventListener("click", handlerClick);
+function handlerClick(evt) {
+  evt.preventDefault();
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
